dae42c3de95ffa828e29bd77110a35fb9041a18a
Caio Uehara <caioueharamartins@gmail.com>
2023-10-23 16:46:59 -0300

    Add update, deleteByPK

--- src/controllers/client.controller.js
+++ src/controllers/client.controller.js
@@ -3,26 +3,52 @@ import Client from '../model/client.model.js'
 let findAll = (request, response) => {
     Client.findAll().then( results => {
         response.status(200).json(results)
-    }).catch(error => {
-        console.log(error)
-    })
+    }).catch(e => { console.log(e) })
 }
 
 let findByPk = (request, response) => {
+    console.log(request.params.id)
     Client.findByPk(request.params.id).then(result => {
         response.status(200).json(result)
-    })
+    }).catch(e => { console.log(e) })
 }
 
-let create = (request, response) => {
+let create = async (request, response) => {
     Client.create({
         name: request.body.name,
         document: request.body.document,
     }).then(result => {
         response.status(201).json(result)
-    }).catch(error => {
-        console.log(error)
-    })
+    }).catch(e => { console.log(e) })
 }
 
-export default {findAll, findByPk, create}
+async function update(request, response) {
+    Client.update({
+        name: request.body.name,
+        document: request.body.document
+    }, {
+        where: {
+            id: request.params.id
+        }
+    }).then(result => {
+        response.status(200).json(result)
+    }).catch((e) => console.log(e))
+}
+
+async function deleteByPk(request, response) {
+    Client.destroy({
+        where: {
+            id: request.params.id
+        }
+    }).then(result => {
+        if (result == 1) {
+            response.status(200).send()
+        } else {
+            response.status(404).send()
+        }
+    }).catch((e) => console.log(e))
+
+}
+
+
+export default {findAll, findByPk, create, update, deleteByPk}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,9 +8,9 @@ routes.get('/breed', breedsController.getRandom)
 routes.get('/breeds/:id', breedsController.getById)
 
 routes.get('/clients', clientController.findAll)
-routes.get('/client/:id', clientController.findByPk)
-routes.post('/client/create', clientController.create)
-//routes.delete('/clients/:id', clientController.deleteByPk)
-//routes.put('/clients/:id', clientController.update)
+routes.get('/clients/:id', clientController.findByPk)
+routes.post('/clients', clientController.create)
+routes.put('/clients/:id', clientController.update)
+routes.delete('/clients/:id', clientController.deleteByPk)
 
 export default routes
